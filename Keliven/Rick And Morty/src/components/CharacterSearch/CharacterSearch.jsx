d6ae3af5317cdae255653012e180263a9aa242e8
gabrielsebrao <gabrielsebrao06@gmail.com>
2024-06-23 19:48:29 -0300

    feat: dynamic search list for rick and morty api

--- Keliven/Rick And Morty/src/components/CharacterSearch/CharacterSearch.jsx
+++ Keliven/Rick And Morty/src/components/CharacterSearch/CharacterSearch.jsx
@@ -6,46 +6,38 @@ export default function CharacterSearch() {
     const [apiUsers, setApiUsers] = useState([])
     const [loading, setLoading] = useState(true)
     const [name, setName] = useState("")
-    const [filteredUsers, setFilteredUsers] = useState(apiUsers)
 
     useEffect(() => {
-        fetch('https://dummyjson.com/users')
+        fetch(`https://rickandmortyapi.com/api/character/?name=${name}`)
             .then(response => response.json())
             .then(data => {
-                setApiUsers(data.users)
-                setFilteredUsers(data.users)
+                setApiUsers(data.results || [])
             })
             .catch(err => console.log(err))
             .finally(() => {
                 setLoading(false)
             })
-    }, [])
+    }, [name])
 
     const handleInputChange = (e) => {
         const searchTerm = e.target.value;
         setName(searchTerm)
-        console.log(apiUsers)
-        const filteredItems = apiUsers.filter((user) =>
-            user.firstName.toLowerCase().includes(searchTerm.toLowerCase())
-        )
-        setFilteredUsers(filteredItems)
     }
 
     return (
-        <>
-            <h1>Rick and Morty</h1>
+        <div className="CharacterSearch">
             <input
                 type="text"
                 value={name}
                 onChange={handleInputChange}
-                placeholder='Type to search'
+                placeholder='Type to search a character'
             />
-            {!loading && filteredUsers.length === 0
+            {!loading && apiUsers.length === 0
                 ? <p>No users found</p>
                 : <ul>
-                    {filteredUsers.map(user => <li key={user.id}>{user.firstName}</li>)}
+                    {apiUsers.map(user => <li key={user.id}>{user.name} <img src={user.image}></img></li>)}
                 </ul>
             }
-        </>
+        </div>
     )
 }
